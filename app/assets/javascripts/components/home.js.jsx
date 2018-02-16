78fc2da7d5409bc374ef1d0278ed4cbf112aad55
Roydon <roydon.quadros@gmail.com>
2018-02-16 13:07:22 +0530

    Added feeds model

--- app/assets/javascripts/components/home.js.jsx
+++ app/assets/javascripts/components/home.js.jsx
@@ -1,8 +1,16 @@
 class Home extends React.Component {
   render() {
     return (
-      <div>
-        <h1>Welcome to your Profile !</h1>
+      <div className = "container-fluid">
+        <div className = "row">
+          <div className = "col-md-4">
+            <h1>Name - </h1>
+            <h2>Feeds - </h2>
+          </div>
+          <div className = "col-md-8">
+            <h1>Feeds&nbsp;<a href="/feeds/add">Add Feed</a></h1>
+          </div>
+        </div>
       </div>
     );
   }
---
+++ app/models/feed.rb
@@ -0,0 +1,5 @@
+class Feed < ApplicationRecord
+  belongs_to :user
+  validates :user_id , presence: true
+  validates :content , presence: true
+end
